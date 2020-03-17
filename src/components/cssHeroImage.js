7b2193e479ec1ad26d6d790ccd77a4394961738c
Claudia Estrada <cgestrada518@gmail.com>
2020-03-17 14:39:42 -0700

    fix page width glitch

--- src/components/cssHeroImage.js
+++ src/components/cssHeroImage.js
@@ -3,7 +3,7 @@ import heroImageStyles from "./cssHeroImage.module.css"
 
 const CssHeroImage = () => {
   return (
-    <div>
+    <div className={heroImageStyles.Container}>
       <div className={heroImageStyles.Line}></div>
       <div className={heroImageStyles.Triangle}></div>
     </div>

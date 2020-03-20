8fc2462083ace4052bfaa540f0636c0d645c5a5d
Claudia Estrada <cgestrada518@gmail.com>
2020-03-19 20:58:56 -0700

    create menu component

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,44 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import headerStyles from "./headerStyles.module.css"
 
+import Menu from "./menu"
 import Logo from "../images/manceps-triangle.png"
 
 const Header = () => {
+  const [menuClicked, setMenuClicked] = useState(false)
+
   const handleMenuClick = () => {
-    return (
-      <div className={headerStyles.Menu}>
-        <h1>This is the menu.</h1>
-      </div>
-    )
+    if (menuClicked) {
+      setMenuClicked(false)
+    } else {
+      setMenuClicked(true)
+    }
   }
 
   return (
-    <header>
-      <a href="https://www.manceps.ai/">
-        <img src={Logo} alt="Manceps Logo" className={headerStyles.Logo} />
-      </a>
-      <div
-        role="button"
-        tabIndex="0"
-        className={headerStyles.Burger}
-        id="burger"
-        onClick={handleMenuClick}
-        onKeyPress={handleMenuClick}
-      >
-        <div className={headerStyles.Lines}>
-          <div className={headerStyles.Line}></div>
-          <div className={headerStyles.Line}></div>
-          <div className={headerStyles.Line}></div>
+    <React.Fragment>
+      {menuClicked ? <Menu /> : null}
+      <header>
+        <a href="https://www.manceps.ai/">
+          <img src={Logo} alt="Manceps Logo" className={headerStyles.Logo} />
+        </a>
+        <div
+          role="button"
+          tabIndex="0"
+          className={headerStyles.Burger}
+          id="burger"
+          onClick={handleMenuClick}
+          onKeyPress={handleMenuClick}
+        >
+          <div className={headerStyles.Lines}>
+            <div className={headerStyles.Line}></div>
+            <div className={headerStyles.Line}></div>
+            <div className={headerStyles.Line}></div>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </React.Fragment>
   )
 }
 
---
+++ src/components/menu.js
@@ -0,0 +1,8 @@
+import React from "react"
+import menuStyles from "./menu.module.css"
+
+const Menu = () => {
+  return <div className={menuStyles.Container}></div>
+}
+
+export default Menu
